eea6c8739c1132fc1b567d54b0defad4b461d057
Luan Gabriel <luangabriel27@icloud.com>
2026-07-11 01:32:20 -0300

    T03_Completed

--- T04/PJ_02/le_MARS/strcpyMARS_PJ02.asm
+++ T04/PJ_02/le_MARS/strcpyMARS_PJ02.asm
@@ -1,19 +1,17 @@
 .data
-# A estrutura que você definiu:
 .data
-# The MARS data space starts at address 0x10010000
 dest_text_addr: .word 0
    .space 9200 # This completes the reserved quasi-9Kbytes space for the source string
 
 after_dest:  .word 0 # After the string end, put a label, to enable visualizing the end of the data segment window
-   .space 7144 # jump to align to address 0x10014000
-
-filename:  .asciiz "D:/Maquinas Virtuais/Xilinx_ISE_14.7_Win10_14.7_VM_0213_1/Ise_projs/CCS_MIPS_S_withBRAMs/T04/PJ_02/le_MARS/RomeoJuliet_Act2-Scene2.txt" # filename at the end, to facilitate…
+   .space 7176 # jump to align to address 0x10014000
 
 source_text_addr: .word 0 # After the string end, put a label, to enable visualizing the end of the data segment window
    .space 9200 # This completes the reserved quasi-9Kbytes for the destination string
 
 after_source:  .word 0 # After the string end, put a label, to enable visualizing the end of the data segment window
+
+filename:  .asciiz "/Users/luan/Desktop/PJ_02/le_MARS/RomeoJuliet_Act2-Scene2.txt" # filename at the end, to facilitate
 
 .text
 .globl main
@@ -23,18 +21,18 @@ main:
     jal carrega_arquivo
 
     # 2. Prepara argumentos para a funcao strcpy
-    la $a0, source_text_addr  # $a0 = Ponteiro da Fonte (Garante o endereço 0x10014000)
-    la $a1, dest_text_addr    # $a1 = Ponteiro do Destino (Garante o endereço 0x10010000)
+    la $a0, source_text_addr  # $a0 = Ponteiro da Fonte (Garante o endereï¿½o 0x10014000)
+    la $a1, dest_text_addr    # $a1 = Ponteiro do Destino (Garante o endereï¿½o 0x10010000)
     jal strcpy
 
     # --- TRECHO DE DEBUG: Imprimir o que foi lido na tela ---
     # li $v0, 4                 # Syscall 4: Print String
-    # la $a0, source_text_addr  # Endereço de onde o texto foi salvo (0x10014000)
+    # la $a0, source_text_addr  # Endereï¿½o de onde o texto foi salvo (0x10014000)
     # syscall
     # --------------------------------------------------------
 
-    # 3. Fim da execução (Projeto 2)
-    # Nota: No Projeto 3, isso deverá ser trocado por um "j aqui" (loop eterno).
+    # 3. Fim da execuï¿½ï¿½o (Projeto 2)
+    # Nota: No Projeto 3, isso deverï¿½ ser trocado por um "j aqui" (loop eterno).
     li $v0, 10
     syscall
 
@@ -42,25 +40,25 @@ main:
 # SUB-ROTINA: carrega_arquivo
 # =========================================================
 carrega_arquivo:
-    # Salva o endereço de retorno na pilha
-    addi $sp, $sp, -4
+    # Salva o endereï¿½o de retorno na pilha
+    addu $sp, $sp, -4
     sw   $ra, 0($sp)
 
     # Syscall 13: Abrir arquivo
-    la $a0, filename          # Endereço da string com o nome do arquivo
+    la $a0, filename          # Endereï¿½o da string com o nome do arquivo
     li $a1, 0                 # Flag 0 = Somente leitura
     li $a2, 0                 # Mode (ignorado no MARS)
     li $v0, 13
     syscall
     move $s0, $v0             # Salva o File Descriptor em $s0
 
-    # Evita ler se o arquivo não existir (File Descriptor < 0)
+    # Evita ler se o arquivo nï¿½o existir (File Descriptor < 0)
     bltz $s0, fim_carrega
 
     # Syscall 14: Ler do arquivo
     move $a0, $s0             # Argumento 1: File Descriptor
-    la   $a1, source_text_addr# Argumento 2: Endereço do buffer (0x10014000)
-    li   $a2, 9200            # Argumento 3: Tamanho máximo a ser lido
+    la   $a1, source_text_addr# Argumento 2: Endereï¿½o do buffer (0x10014000)
+    li   $a2, 9200            # Argumento 3: Tamanho mï¿½ximo a ser lido
     li   $v0, 14
     syscall
 
@@ -70,9 +68,9 @@ carrega_arquivo:
     syscall
 
 fim_carrega:
-    # Restaura o endereço de retorno e volta para a main
+    # Restaura o endereï¿½o de retorno e volta para a main
     lw   $ra, 0($sp)
-    addi $sp, $sp, 4
+    addu $sp, $sp, 4
     jr   $ra
 
 # =========================================================
@@ -80,13 +78,13 @@ fim_carrega:
 # =========================================================
 strcpy:
 loop_strcpy:
-    lb   $t0, 0($a0)          # Lê 1 byte do endereço Fonte
-    sb   $t0, 0($a1)          # Escreve esse byte no endereço Destino
+    lb   $t0, 0($a0)          # Lï¿½ 1 byte do endereï¿½o Fonte
+    sb   $t0, 0($a1)          # Escreve esse byte no endereï¿½o Destino
     
     beq  $t0, $zero, fim_strcpy # Verifica fim da string (NULL / 0x00)
     
-    addi $a0, $a0, 1          # Incrementa ponteiro da Fonte
-    addi $a1, $a1, 1          # Incrementa ponteiro do Destino
+    addu $a0, $a0, 1          # Incrementa ponteiro da Fonte
+    addu $a1, $a1, 1          # Incrementa ponteiro do Destino
     j    loop_strcpy          # Repete
     
 fim_strcpy:
--- T04/PJ_03/le_MARS/strcpyMARS_PJ_03.asm
+++ T04/PJ_03/le_MARS/strcpyMARS_PJ_03.asm
@@ -1,22 +1,65 @@
 .data
-dest_text_addr: .word 0x10010000 
+.data
+dest_text_addr: .word 0
    .space 9200 
+
 after_dest:  .word 0 
-   .space 7144 
-source_text_addr: .word 0x10014000 
+   .space 7176 
+
+source_text_addr: .word 0 
    .space 9200 
+
 after_source:  .word 0 
+
+filename:  .asciiz "/Users/luan/Desktop/PJ_02/le_MARS/RomeoJuliet_Act2-Scene2.txt" 
 
 .text
 .globl main
 
 main:
+    jal carrega_arquivo
+
     la $a0, source_text_addr  
     la $a1, dest_text_addr    
     jal strcpy
 
-aqui: 
-    j aqui 
+    # --- TRECHO DE DEBUG: Imprimir o que foi lido na tela ---
+    # li $v0, 4                 # Syscall 4: Print String
+    # la $a0, source_text_addr  # Endere�o de onde o texto foi salvo (0x10014000)
+    # syscall
+    # --------------------------------------------------------
+aqui: j aqui
+
+carrega_arquivo:
+    addu $sp, $sp, -4
+    sw   $ra, 0($sp)
+
+    
+    la $a0, filename          
+    li $a1, 0                 
+    li $a2, 0                 
+    li $v0, 13
+    syscall
+    move $s0, $v0             
+    
+    bltz $s0, fim_carrega
+
+    move $a0, $s0             
+    la   $a1, source_text_addr
+    li   $a2, 9200            
+    li   $v0, 14
+    syscall
+
+    move $a0, $s0
+    li   $v0, 16
+    syscall
+
+fim_carrega:
+
+    lw   $ra, 0($sp)
+    addu $sp, $sp, 4
+    jr   $ra
+
 
 strcpy:
 loop_strcpy:
@@ -27,7 +70,9 @@ loop_strcpy:
     
     addu $a0, $a0, 1          
     addu $a1, $a1, 1          
-    j    loop_strcpy          
+    j    loop_strcpy        
     
 fim_strcpy:
-    jr   $ra
+    jr   $ra 
+
+    
